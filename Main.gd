f58d5080c79ad6cf4227eec58ab4939bd9e1cd2b
Yegor Vaskonyan <egor.vaskoyan@gmail.com>
2023-01-12 10:00:06 +0100

    hh

--- Main.gd
+++ Main.gd
@@ -11,7 +11,7 @@ func _input(event):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Lift.position = $LiftPos.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
