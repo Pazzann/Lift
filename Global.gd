03abdc0a66deb70e4cab35f53a9b8c65a34f0b69
YUR3Y <yura.4ernenko@gmail.com>
2023-01-12 11:03:35 +0300

    fdh

---
+++ Global.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var MAX_QUEUE_COUNT = 0
+var MAX_QUEUE_TIME = 0
+var PLACE_COUNT = 0
+var REPAIR_TIME = 0
+var HAPPY_TIME = 0
+var GEN_TIME = 0
+var TARGET_THANKS = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
