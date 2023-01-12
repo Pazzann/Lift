875858fff9d1b7c56c3c1b18d42002af6bd5f370
Anton <manton2007@gmail.com>
2023-01-12 11:25:08 +0200

    omg

--- Scripts/Liver.gd
+++ Scripts/Liver.gd
@@ -1,18 +1,17 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var time = 0
 var happyTime = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	happyTime = get_node("/root/Constants")
-	pass # Replace with function body.
+	happyTime = Global.MAX_QUEUE_COUNT
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	time += delta;
+	
+func _checkLeave():
+	pass
