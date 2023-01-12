7b129e654999e0c1214ce9cad6aa72ecee493d59
denyshon <mr.denis.goncharov@gmail.com>
2023-01-12 14:46:21 +0200

    Lost Scene added s

--- Lost.gd
+++ Lost.gd
@@ -14,13 +14,9 @@ func _input(event):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_tree().change_scene("Main.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Restart_pressed():
-	get_tree().change_scene("Main.tscn")
